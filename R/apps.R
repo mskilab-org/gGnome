c204fb2a50e95444effe4d1d9f5a0bd65c27583d
Zi-Ning Choo <zining01@gmail.com>
2021-04-01 16:36:03 -0400

    switch to SD

--- R/apps.R
+++ R/apps.R
@@ -2264,7 +2264,7 @@ phased.binstats = function(gg, bins = NULL, purity = NULL, ploidy = NULL,
     phased.gg.nodes$weight = ifelse((phased.gg.nodes$var > 0) &
                                     (phased.gg.nodes$nbins > min.bins) &
                                     (phased.gg.nodes$var > min.var),
-                                    phased.gg.nodes$nbins / phased.gg.nodes$var,
+                                    (phased.gg.nodes$nbins / sqrt(phased.gg.nodes$var)),
                                     NA)
 
     if (verbose) {
