6b80f90c1fb770626c088726a809decaf3c5a2c8
ShaiberAlon <alon.shaiber@gmail.com>
2021-09-28 17:45:52 -0400

    added tests for rck2gg

--- tests/testthat/test_gGnome_converters.R
+++ tests/testthat/test_gGnome_converters.R
@@ -18,3 +18,36 @@ test_that('cougar2gg', {
     expect_error(cougar2gg('not a dir'))
     expect_error(cougar2gg('./')) # not a cougar output directory
 })
+
+test_that('rck2gg', {
+    # download cougar files from cougar repo
+
+    rck.tar = system.file('extdata', "rck.hcc1954.tar.gz", package = "gGnome")
+    system(paste0('tar -xzvf ', rck.tar))
+    rck.dir = 'rck.hcc1954'
+
+    haploid.res = rck2gg(rck.dir, haploid = TRUE)
+    haploid.gg = gG(nodes = haploid.res$nodes, edges = haploid.res$edges)
+    expect_true(length(haploid.res$edges) == 10)
+    expect_true(inherits(haploid.gg, 'gGraph'))
+
+    haploid.res = rck2gg(rck.dir, haploid = FALSE)
+    haploid.gg.not.haploid = gG(nodes = haploid.res$nodes, edges = haploid.res$edges)
+    expect_true(length(haploid.res$edges) == 14)
+    expect_true(inherits(haploid.gg.not.haploid, 'gGraph'))
+
+    expect_error(rck2gg('not a dir'))
+    expect_error(rck2gg('./')) # not a cougar output directory
+
+    # make bad rck files
+    tmp = paste0(tempdir(), '/bad.rck')
+    dir.create(tmp)
+    fwrite(data.table(V1 = 'bad', V2 = 'not better'), paste0(tmp, '/rck.scnt.tsv'))
+    fwrite(data.table(V1 = 'bad', V2 = 'not better'), paste0(tmp, '/rck.acnt.tsv'))
+    expect_error(rck2gg(tmp))
+
+    # good scnt file, bad acnt file
+    good_scnt = paste0(rck.dir, '/rck.scnt.tsv')
+    system(paste0('cp ', good_scnt, ' ', tmp))
+    expect_error(rck2gg(tmp))
+})
