e372973c0c87bac88f93bff1390686ca78550a21
Tanubrata Dey <tdey@nygenome.org>
2023-09-20 10:20:07 -0400

    Added some fix for testing CPLEX

--- R/test_CPLEX.R
+++ R/test_CPLEX.R
@@ -5,7 +5,7 @@
 #' 
 #' @return returns a message whether the check was successful, else does a fresh installation of gGnome 
 #' @author Tanubrata Dey
-#'  Allows for subsetting of the gEdge Object using bracket notation
+#'  
 
 
 testOptimizationFunction <- function() {
@@ -21,22 +21,23 @@ testOptimizationFunction <- function() {
   tryCatch({
                                         # Call the optimization function with the default matrix
     gGnome::Rcplex2(cvec, Amat, bvec, Qmat = defaultQmat)
-    message("Optimization successful.\n")
+    message("CPLEX has been wired up with gGnome.\n")
                                         # You can add additional checks on the result here if needed
   }, error = function(e) {
                                         # If there is an error, install the gGnome package
     message("CPLEX is not wired with gGnome, Make sure CPLEXDIR exists in your .bashrc to add CPLEX functionality in gGnome.")
     cplex.dir = Sys.getenv("CPLEX_DIR")
     if (is.null(cplex.dir)){
-      stop("CPLEX_DIR environment variable not found!")
+      stop("CPLEX_DIR environment variable not found! Check your .bashrc...\n")
     } else if (!file.exists(paste0(cplex.dir, "/cplex"))) {
       stop("${CPLEX_DIR}/cplex not found")
     } else if (!file.exists(paste0(cplex.dir, "/cplex/include")) ||
                !file.exists(paste0(cplex.dir, "/cplex/lib"))){
       stop("${CPLEX_DIR}/cplex/[(include)|(lib)] do not both exist")
     } else {
-      message("Force re-installing gGnome to add CPLEX...")
-      devtools::install_github("mskilab-org/gGnome", force = TRUE)
+      message("gGnome does not have CPLEX wired. Force re-installing gGnome to add CPLEX...")
+      #devtools::install_github("mskilab-org/gGnome", force = TRUE)
+      devtools::install_github("tanubrata/gGnome", force = TRUE)
       #install.packages("~/git/gGnome_0.1.tar.gz", force = TRUE)
     }
   })
