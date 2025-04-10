49f7dd1ed96b983d7356b0467c35a57de940b3a8
kevinmhadi <kevinmhadi.gmail.com>
2025-04-10 16:34:21 -0400

    Robustifying a transpose method used in fusions(). data.table::transpose() is unreliable due to version changing.

--- R/eventCallers.R
+++ R/eventCallers.R
@@ -1445,7 +1445,7 @@ get_txloops = function(tgg,
           list(list(list(mincn = min_val, maxcn = max_val)))
         }
       ) 
-      list_of_cn = data.table::transpose(list_of_cn)
+      list_of_cn = gGnome::transpose(list_of_cn)
       for (i in seq_along(list_of_cn)) {
         list_of_cn[[i]] = unlist(list_of_cn[[i]])
       }      
